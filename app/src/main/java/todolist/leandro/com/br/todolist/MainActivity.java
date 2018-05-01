91dba7bf08232a643a49c156324b5c4ddabb7f83
leandro <leandrocgms1986@outlook.com>
2018-05-01 12:39:53 -0400

    II parte do desenvolvimento desse app.

--- app/src/main/java/todolist/leandro/com/br/todolist/MainActivity.java
+++ app/src/main/java/todolist/leandro/com/br/todolist/MainActivity.java
@@ -7,9 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +21,9 @@ public class MainActivity extends Activity {
     private Button botaoAdicionar;
     private ListView listaTarefas;
     private SQLiteDatabase bancoDados;
+
+    private ArrayAdapter<String> itensAdaptador;
+    private ArrayList<String> itens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,6 @@ public class MainActivity extends Activity {
             // Recuperar componentes
             textoTarefa = findViewById(R.id.textoId);
             botaoAdicionar = findViewById(R.id.botaoAdicionarId);
-            listaTarefas = findViewById(R.id.listViewId);
 
             // Banco dados
             bancoDados = openOrCreateDatabase("apptarefas", MODE_PRIVATE, null);
@@ -39,27 +45,70 @@ public class MainActivity extends Activity {
                 public void onClick(View v) {
 
                     String textoDigitado = textoTarefa.getText().toString();
-                    bancoDados.execSQL("INSERT INTO tarefas(tarefa) VALUES('" + textoDigitado + "')");
+                    salvarTarefa(textoDigitado);
 
                 }
             });
 
-            Cursor cursor = bancoDados.rawQuery("SELECT * FROM tarefas ", null);
+            // listar tarefas
+            recuperarTarefas();
 
-            // recuperar os ids das colunas
-            int indiceColuna = cursor.getColumnIndex("id");
-            int indiceColunaTarefa = cursor.getColumnIndex("tarefa");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-            // listar as tarefas
-            cursor.moveToFirst();
-            while (cursor != null) {
-                Log.i("Resultado - ", "Tarefa: " + cursor.getString(indiceColunaTarefa));
-                cursor.moveToNext();
+    }
+
+    private void salvarTarefa(String texto) {
+
+        try {
+
+            if (texto.equals("")) {
+                Toast.makeText(MainActivity.this, "O campo da tarefa não pode" +
+                        "estar vazio.", Toast.LENGTH_SHORT).show();
+            } else {
+                bancoDados.execSQL("INSERT INTO tarefas(tarefa) VALUES('" + texto + "')");
+                Toast.makeText(MainActivity.this, "Tarefa salva com sucesso.",
+                        Toast.LENGTH_SHORT).show();
+                recuperarTarefas();
+                textoTarefa.setText("");
+
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    private void recuperarTarefas() {
+
+        try {
+            // Recuperar as tarefas
+            Cursor cursor = bancoDados.rawQuery("SELECT * FROM tarefas ORDER BY id DESC", null);
+
+            // recuperar os ids das colunas
+            int indiceColuna = cursor.getColumnIndex("id");
+            int indiceColunaTarefa = cursor.getColumnIndex("tarefa");
+
+            // lista
+            listaTarefas = findViewById(R.id.listViewId);
+
+            // Criar adaptador
+            itens = new ArrayList<String>();
+            itensAdaptador = new ArrayAdapter<String>(getApplicationContext(),
+                    android.R.layout.simple_list_item_2, android.R.id.text2, itens);
+            listaTarefas.setAdapter(itensAdaptador);
+
+            // listar as tarefas
+            cursor.moveToFirst();
+            while (cursor != null) {
+                Log.i("Resultado - ", "Tarefa: " + cursor.getString(indiceColunaTarefa));
+                itens.add( cursor.getString(indiceColunaTarefa) );
+                cursor.moveToNext();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
